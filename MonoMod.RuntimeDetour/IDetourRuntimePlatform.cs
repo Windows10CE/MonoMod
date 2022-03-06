48b83facf74a78df65381780f0e9dec4b89a97fd
Jade Macho <nuda1998@gmail.com>
2022-03-06 14:20:59 +0100

    Undo MonoMod.Common

---
+++ MonoMod.RuntimeDetour/IDetourRuntimePlatform.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using MonoMod.Utils;
+using System.Linq;
+
+namespace MonoMod.RuntimeDetour {
+    public interface IDetourRuntimePlatform {
+        MethodBase GetIdentifiable(MethodBase method);
+        IntPtr GetNativeStart(MethodBase method);
+        MethodInfo CreateCopy(MethodBase method);
+        bool TryCreateCopy(MethodBase method, out MethodInfo dm);
+        void Pin(MethodBase method);
+        void Unpin(MethodBase method);
+        MethodBase GetDetourTarget(MethodBase from, MethodBase to);
+        uint TryMemAllocScratchCloseTo(IntPtr target, out IntPtr ptr, int size);
+
+        bool OnMethodCompiledWillBeCalled { get; }
+        event OnMethodCompiledEvent OnMethodCompiled;
+    }
+
+    public delegate void OnMethodCompiledEvent(MethodBase method, IntPtr codeStart, ulong codeSize);
+}
---
+++ MonoMod.Utils/ICallSiteGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Linq.Expressions;
+using MonoMod.Utils;
+using System.Collections.Generic;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace MonoMod.Utils {
+    public interface ICallSiteGenerator {
+
+        CallSite ToCallSite(ModuleDefinition module);
+
+    }
+}
